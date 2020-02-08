7f16a2cd0d3bfe31763fe14c849e3482b488968a
JChamblee99 <jtc38@students.uwf.edu>
2020-02-07 22:12:57 -0600

    Cleaned conn_string statement

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-	$conn_string = "pgsql:host=ec2-184-72-236-57.compute-1.amazonaws.com;dbname=d9eig3e03gvtfj";
+	$host = "ec2-184-72-236-57.compute-1.amazonaws.com";
+	$db_name = "d9eig3e03gvtfj";
+	$conn_string = "pgsql:host=".$conn_string.";dbname=".$db_name;
+
 	$user = "bbvtmoyxqqsrbk";
 	$password = "40fe9b6dc6a9b090eb05c5b17b5a9e0be4896301ce9b8a52b169e710db66a758";
 	$myPDO = new PDO($conn_string, $user, $password);
