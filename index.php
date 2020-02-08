beae0c78f82424771c4cb1f4a59f2e8bc0f2f5dc
JChamblee99 <jtc38@students.uwf.edu>
2020-02-07 22:24:58 -0600

    Added comments

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
 <?php
+	// Connection details
 	$host = "ec2-184-72-236-57.compute-1.amazonaws.com";
 	$db_name = "d9eig3e03gvtfj";
 	$conn_string = "pgsql:host=".$host.";dbname=".$db_name;
 
+	// Connect
 	$user = "bbvtmoyxqqsrbk";
 	$password = "40fe9b6dc6a9b090eb05c5b17b5a9e0be4896301ce9b8a52b169e710db66a758";
 	$myPDO = new PDO($conn_string, $user, $password);
 
+	// Create sensors table
 	$myPDO->query('CREATE TABLE IF NOT EXISTS sensors (accel_x real, accel_y real, accel_z real, quat_x real, quat_y real, quat_z real, quat_w real)');
 
+	// Prepare insertion statement
 	$insert_string = 'INSERT INTO sensors (accel_x, accel_y, accel_z, quat_x, quat_y, quat_z, quat_w) VALUES (?, ?, ?, ?, ?, ?, ?)';
 	$insert = $myPDO->prepare($insert_string);
 
 
+	// Convert hex->data->json and then execute with prepared statement
 	$raw_post = hexToStr($_GET['data']);
 	$json_data = json_decode($raw_post, TRUE);
 	foreach($json_data['data'] as $line) {
@@ -21,6 +26,9 @@
 	}
 
 	echo "Success!";
+
+
+
 
 	function hexToStr($hex){
 		$string='';
