8759e19d774a78922608cbfb3cfa725194f23139
JChamblee99 <jtc38@students.uwf.edu>
2020-02-07 23:53:03 -0600

    Fixed multiple row insertion issue

--- index.php
+++ index.php
@@ -7,34 +7,38 @@
 	// Connect
 	$user = "bbvtmoyxqqsrbk";
 	$password = "40fe9b6dc6a9b090eb05c5b17b5a9e0be4896301ce9b8a52b169e710db66a758";
-	$myPDO = new PDO($conn_string, $user, $password);
 
-	// Create sensors table
-	$myPDO->query('CREATE TABLE IF NOT EXISTS sensors (accel_x real, accel_y real, accel_z real, quat_x real, quat_y real, quat_z real, quat_w real)');
+	try {
+		$myPDO = new PDO($conn_string, $user, $password);
 
-	// Prepare insertion statement
-	$insert_string = 'INSERT INTO sensors (accel_x, accel_y, accel_z, quat_x, quat_y, quat_z, quat_w) VALUES (:accel_x, :accel_y, :accel_z, :quat_x, :quat_y, :quat_z, :quat_w)';
-	$insert = $myPDO->prepare($insert_string);
+		// Create sensors table
+		$myPDO->query('CREATE TABLE IF NOT EXISTS sensors (accel_x real, accel_y real, accel_z real, quat_x real, quat_y real, quat_z real, quat_w real)');
+
+		// Prepare insertion statement
+		$insert_string = 'INSERT INTO sensors (accel_x, accel_y, accel_z, quat_x, quat_y, quat_z, quat_w) VALUES (:accel_x, :accel_y, :accel_z, :quat_x, :quat_y, :quat_z, :quat_w)';
+		$insert = $myPDO->prepare($insert_string);
 
 
-	// Convert hex->data->json and then execute with prepared statement
-	$raw_post = hexToStr($_GET['data']);
-	$json_data = json_decode($raw_post, TRUE);
-	foreach($json_data['data'] as $line) {
-		$insert->bindParam(':accel_x',$line['accel_x']);
-		$insert->bindParam(':accel_y',$line['accel_y']);
-		$insert->bindParam(':accel_z',$line['accel_z']);
-		$insert->bindParam(':quat_x',$line['quat_x']);
-		$insert->bindParam(':quat_y',$line['quat_y']);
-		$insert->bindParam(':quat_z',$line['quat_z']);
-		$insert->bindParam(':quat_w',$line['quat_w']);
+		// Convert hex->data->json and then execute with prepared statement
+		$raw_post = hexToStr($_GET['data']);
+		$json_data = json_decode($raw_post, TRUE);
+		foreach($json_data['data'] as $line) {
+			$insert->bindParam(':accel_x',$line['accel_x']);
+			$insert->bindParam(':accel_y',$line['accel_y']);
+			$insert->bindParam(':accel_z',$line['accel_z']);
+			$insert->bindParam(':quat_x',$line['quat_x']);
+			$insert->bindParam(':quat_y',$line['quat_y']);
+			$insert->bindParam(':quat_z',$line['quat_z']);
+			$insert->bindParam(':quat_w',$line['quat_w']);
 
-		$insert->execute();
+			$insert->execute();
+		}
+
+		echo "New records created successfully!";
+
+	} catch (PDOException $e) {
+		echo "Error: ".$e->getMessage();
 	}
-
-	echo "Success!";
-
-
 
 
 	function hexToStr($hex){
