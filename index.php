c87bca10beb306d7610730ab96f2eb7e05c0a6e4
JChamblee99 <jtc38@students.uwf.edu>
2020-02-07 20:58:53 -0600

    Experimenting with Postgresql

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 	//$conn_string = "host=ec2-184-72-236-57.compute-1.amazonaws.com port=5432 dbname=d9eig3e03gvtfj user=bbvtmoyxqqsrbk password=40fe9b6dc6a9b090eb05c5b17b5a9e0be4896301ce9b8a52b169e710db66a758"
 	//$db = pg_connect($conn_string) or die("Connection to database failed!");
 
-	$conn_string = "pgsql:host=ec2-184-72-236-57.compute-1.amazonaws.com;dbname=d9eig3e03gvtfj" 
+	$conn_string = "pgsql:host=ec2-184-72-236-57.compute-1.amazonaws.com;dbname=d9eig3e03gvtfj";
 	$user = "bbvtmoyxqqsrbk";
 	$password = "40fe9b6dc6a9b090eb05c5b17b5a9e0be4896301ce9b8a52b169e710db66a758"
 	$myPDO = new PDO($conn_string, $user, $password);
