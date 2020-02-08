3d38b1b8f916e30328c13ac35dba0d6c4837798c
JChamblee99 <jtc38@students.uwf.edu>
2020-02-07 20:50:14 -0600

    Experimenting with Postgresql

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
-	$conn_string = "host=ec2-184-72-236-57.compute-1.amazonaws.com port=5432 dbname=d9eig3e03gvtfj user=bbvtmoyxqqsrbk password=40fe9b6dc6a9b090eb05c5b17b5a9e0be4896301ce9b8a52b169e710db66a758"
+	/*$conn_string = "host=ec2-184-72-236-57.compute-1.amazonaws.com port=5432 dbname=d9eig3e03gvtfj user=bbvtmoyxqqsrbk password=40fe9b6dc6a9b090eb05c5b17b5a9e0be4896301ce9b8a52b169e710db66a758"
 	$db = pg_connect($conn_string) or die("Connection to database failed!");
 
 	$raw_post = hexToStr($_GET["data"]);
-	/*$lines = explode('\n', $raw_post);
+	$lines = explode('\n', $raw_post);
 	foreach($lines as $json_line) {
 		$line = json_decode($json_line);
 	}*/
-
+	echo "test";
 
 	function hexToStr($hex){
 		$string='';
