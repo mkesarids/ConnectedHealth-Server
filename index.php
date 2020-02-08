afd3ba16f5eee8243a64789e6eb71ad7947953ac
JChamblee99 <jtc38@students.uwf.edu>
2020-02-07 21:33:26 -0600

    Experimenting with Postgresql

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 
 	$raw_post = hexToStr($_GET['data']);
 	echo "raw: ";
-	echo $raw_post + "\n";
+	echo $raw_post;
+	echo "\n";
 	$json_data = json_decode($raw_post, TRUE);
 	//$lines = explode('\n', $raw_post);
 	foreach($json_data['data'] as $line) {
