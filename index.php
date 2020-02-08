13890088fe987ca7cd23e9cf391f33e90079d20b
JChamblee99 <jtc38@students.uwf.edu>
2020-02-07 18:42:20 -0600

    Added basic I/O test

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-	echo "Working";
+	$file = fopen("data.csv", "w") or die("Unable to open file!");
+	fwrite($file, "test,test\n");
+	fclose($file);
+
+	$file = fopen("data.csv", "r") or die("Unable to open file!");
+	echo fread($file, filesize("data.csv"));
+	flose($file);
 ?>
