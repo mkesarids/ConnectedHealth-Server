54d0f4cc3fdde586cf211e30d8a4b3742234586e
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2020-03-04 14:47:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,9 @@
 		// Create sensors table
 		$myPDO->exec('CREATE TABLE sensors (record_id text, name text, accel_x real, accel_y real, accel_z real, quat_x real, quat_y real, quat_z real, quat_w real, workout text)');
 
+		$insert_string = 'INSERT INTO sensors (record_id, user, accel_x, accel_y, accel_z, quat_x, quat_y, quat_z, quat_w, workout) VALUES (:record_id, :user, :accel_x, :accel_y, :accel_z, :quat_x, :quat_y, :quat_z, :quat_w, :workout)';
+		$insert = $myPDO->prepare($insert_string);
+		
 		// Convert hex->data->json and then execute with prepared statement
 		$raw_post = hexToStr($_GET['data']);
 		$json_data = json_decode($raw_post, TRUE);
