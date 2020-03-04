59c8119a01244508e1b15f4f98e7159f05dad887
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2020-03-04 15:09:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 		$myPDO = new PDO($conn_string, $user, $password);
 
 		// Create sensors table
-		$myPDO->exec('CREATE TABLE IF NOT EXIST sensors (record_id text, name text, accel_x real, accel_y real, accel_z real, quat_x real, quat_y real, quat_z real, quat_w real, workout text)');
+		$myPDO->exec('CREATE TABLE sensors (record_id text, name text, accel_x real, accel_y real, accel_z real, quat_x real, quat_y real, quat_z real, quat_w real, workout text)');
 
 		$insert_string = 'INSERT INTO sensors (record_id, name, accel_x, accel_y, accel_z, quat_x, quat_y, quat_z, quat_w, workout) VALUES (:record_id, :name, :accel_x, :accel_y, :accel_z, :quat_x, :quat_y, :quat_z, :quat_w, :workout)';
 		$insert = $myPDO->prepare($insert_string);
