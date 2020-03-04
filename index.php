f215fafdf5839b052ebd96a54f190807aed4bd0f
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2020-03-04 15:00:08 -0600

    Update index.php

--- index.php
+++ index.php
@@ -13,8 +13,25 @@
 
 		// Create sensors table
 		$myPDO->exec('CREATE TABLE sensors (record_id text, name text, accel_x real, accel_y real, accel_z real, quat_x real, quat_y real, quat_z real, quat_w real, workout text)');
-		$myPDO->exec('INSERT INTO sensors (record_id) VALUES (12432236);');
+
+		$insert_string = 'INSERT INTO sensors (record_id, name, accel_x, accel_y, accel_z, quat_x, quat_y, quat_z, quat_w, workout) VALUES (:record_id, :name, :accel_x, :accel_y, :accel_z, :quat_x, :quat_y, :quat_z, :quat_w, :workout)';
+		$insert = $myPDO->prepare($insert_string);
 		
+		// Convert hex->data->json and then execute with prepared statement
+		$raw_post = hexToStr($_GET['data']);
+		$json_data = json_decode($raw_post, TRUE);
+		foreach($json_data['data'] as $line) {
+			$insert->bindParam(':accel_x',$line['accel_x']);
+			$insert->bindParam(':accel_y',$line['accel_y']);
+			$insert->bindParam(':accel_z',$line['accel_z']);
+			$insert->bindParam(':quat_x',$line['quat_x']);
+			$insert->bindParam(':quat_y',$line['quat_y']);
+			$insert->bindParam(':quat_z',$line['quat_z']);
+			$insert->bindParam(':quat_w',$line['quat_w']);
+
+			$insert->execute();
+		}
+
 		echo "New records created successfully!";
 
 	} catch (PDOException $e) {
