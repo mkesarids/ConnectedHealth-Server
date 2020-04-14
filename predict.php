ff5d4073fad8e6c45fdaad85761393fd370df8f9
JChamblee99 <jtc38@students.uwf.edu>
2020-04-13 20:55:18 -0500

    Corrected input statement for predict

--- predict.php
+++ predict.php
@@ -4,6 +4,7 @@
 	$conn_string = 'pgsql:host='.$configs['host'].';dbname='.$configs['dbname'];
 
 	// Decode json
+	$raw_post = file_get_contents('php://input');
 	$json_data = json_decode($raw_post, TRUE);
 	foreach($json_data['data'] as $sensorData) {
 		echo $sensorData['Acceleration']['X'];
