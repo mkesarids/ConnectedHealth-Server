d32dd8d640c80bacf37cea2e5f070e2850860d46
JChamblee99 <jtc38@students.uwf.edu>
2020-04-13 20:26:59 -0500

    Moved index.php to collect.php

---
+++ collect.php
@@ -0,0 +1,49 @@
+<?php
+	// Connection details
+	$configs = include('config.php');
+	$conn_string = 'pgsql:host='.$configs['host'].';dbname='.$configs['dbname'];
+
+	try {
+		$myPDO = new PDO($conn_string, $configs['username'], $configs['password']);
+
+		// Create sensors table
+		$myPDO->exec('CREATE TABLE sensors (record_id text, timestamp bigint, name text, accel_x real, accel_y real, accel_z real, quat_x real, quat_y real, quat_z real, quat_w real, workout text)');
+
+		$insert_string = 'INSERT INTO sensors (record_id, timestamp, name, accel_x, accel_y, accel_z, quat_x, quat_y, quat_z, quat_w, workout) VALUES (:record_id, :timestamp, :name, :accel_x, :accel_y, :accel_z, :quat_x, :quat_y, :quat_z, :quat_w, :workout)';
+		$insert = $myPDO->prepare($insert_string);
+		
+		// Convert hex->data->json and then execute with prepared statement
+		$raw_post = file_get_contents('php://input'); // hexToStr($_GET['data']);
+		echo $raw_post . "<br>";
+		$json_data = json_decode($raw_post, TRUE);
+		foreach($json_data['data'] as $line) {
+			$insert->bindParam(':record_id',$line['record_id']);
+			$insert->bindParam(':timestamp',$line['timestamp']);
+			$insert->bindParam(':name',$line['name']);
+			$insert->bindParam(':accel_x',$line['accel_x']);
+			$insert->bindParam(':accel_y',$line['accel_y']);
+			$insert->bindParam(':accel_z',$line['accel_z']);
+			$insert->bindParam(':quat_x',$line['quat_x']);
+			$insert->bindParam(':quat_y',$line['quat_y']);
+			$insert->bindParam(':quat_z',$line['quat_z']);
+			$insert->bindParam(':quat_w',$line['quat_w']);
+			$insert->bindParam(':workout',$line['workout']);
+
+			$insert->execute();
+		}
+
+		echo "New records created successfully!";
+
+	} catch (PDOException $e) {
+		echo "Error: ".$e->getMessage();
+	}
+
+
+	function hexToStr($hex){
+		$string='';
+		for ($i=0; $i < strlen($hex)-1; $i+=2){
+			$string .= chr(hexdec($hex[$i].$hex[$i+1]));
+		}
+		return $string;
+	}
+?>
