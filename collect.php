7b26bd4d0c6d17cc7795be6263835db3b0cfd6e9
Benjamin Hendrix <51246821+Bradichus@users.noreply.github.com>
2020-04-15 17:20:09 -0500

    Updated to match new data format

--- collect.php
+++ collect.php
@@ -6,10 +6,12 @@
 	try {
 		$myPDO = new PDO($conn_string, $configs['username'], $configs['password']);
 
-		// Create sensors table
-		$myPDO->exec('CREATE TABLE sensors (record_id text, timestamp bigint, name text, accel_x real, accel_y real, accel_z real, quat_x real, quat_y real, quat_z real, quat_w real, workout text)');
+		// Create sensors table, 
+		// Computer rotation: orient_x/y/z/w
+		// Human rotation in degrees rotate_x/y/z
+		$myPDO->exec('CREATE TABLE sensors (timestamp bigint, session_id bigint, record_id bigint, name text, workout text, accel_x real, accel_y real, accel_z real, orient_x real, orient_y real, orient_z real, orient_w real, rotate_x real, rotate_y real, rotate_z real)');
 
-		$insert_string = 'INSERT INTO sensors (record_id, timestamp, name, accel_x, accel_y, accel_z, quat_x, quat_y, quat_z, quat_w, workout) VALUES (:record_id, :timestamp, :name, :accel_x, :accel_y, :accel_z, :quat_x, :quat_y, :quat_z, :quat_w, :workout)';
+		$insert_string = 'INSERT INTO sensors (timestamp, session_id, record_id, name, workout, accel_x, accel_y, accel_z, orient_x, orient_y, orient_z, orient_w, rotate_x, rotate_y, rotate_z) VALUES (:timestamp, :session_id, :record_id, :name, :workout, :accel_x, :accel_y, :accel_z, :orient_x, :orient_y, :orient_z, :orient_w, :rotate_x, :rotate_y, :rotate_z)';
 		$insert = $myPDO->prepare($insert_string);
 		
 		// Convert hex->data->json and then execute with prepared statement
@@ -17,17 +19,21 @@
 		echo $raw_post . "<br>";
 		$json_data = json_decode($raw_post, TRUE);
 		foreach($json_data['data'] as $line) {
-			$insert->bindParam(':record_id',$line['record_id']);
 			$insert->bindParam(':timestamp',$line['timestamp']);
+			$insert->bindParam(':session_id',$line['session_id']);
+			$insert->bindParam(':record_id',$line['record_id']);
 			$insert->bindParam(':name',$line['name']);
+			$insert->bindParam(':workout',$line['workout']);
 			$insert->bindParam(':accel_x',$line['accel_x']);
 			$insert->bindParam(':accel_y',$line['accel_y']);
 			$insert->bindParam(':accel_z',$line['accel_z']);
-			$insert->bindParam(':quat_x',$line['quat_x']);
-			$insert->bindParam(':quat_y',$line['quat_y']);
-			$insert->bindParam(':quat_z',$line['quat_z']);
-			$insert->bindParam(':quat_w',$line['quat_w']);
-			$insert->bindParam(':workout',$line['workout']);
+			$insert->bindParam(':orient_x',$line['orient_x']);
+			$insert->bindParam(':orient_y',$line['orient_y']);
+			$insert->bindParam(':orient_z',$line['orient_z']);
+			$insert->bindParam(':orient_w',$line['orient_w']);
+			$insert->bindParam(':rotate_x',$line['rotate_x']);
+			$insert->bindParam(':rotate_y',$line['rotate_y']);
+			$insert->bindParam(':rotate_z',$line['rotate_z']);
 
 			$insert->execute();
 		}
