c2295df7303a0326b9c64fc596c4f11fce3654c7
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2020-04-15 18:20:14 -0500

    Update collect.php

--- collect.php
+++ collect.php
@@ -14,26 +14,25 @@
 		$insert_string = 'INSERT INTO sensors (timestamp, session_id, record_id, name, workout, accel_x, accel_y, accel_z, orient_x, orient_y, orient_z, orient_w, rotate_x, rotate_y, rotate_z) VALUES (:timestamp, :session_id, :record_id, :name, :workout, :accel_x, :accel_y, :accel_z, :orient_x, :orient_y, :orient_z, :orient_w, :rotate_x, :rotate_y, :rotate_z)';
 		$insert = $myPDO->prepare($insert_string);
 		
-		// Convert hex->data->json and then execute with prepared statement
-		$raw_post = file_get_contents('php://input'); // hexToStr($_GET['data']);
-		echo $raw_post . "<br>";
+		// Decoding JSON and collecting the data
+		$raw_post = file_get_contents('php://input');
 		$json_data = json_decode($raw_post, TRUE);
-		foreach($json_data['data'] as $line) {
-			$insert->bindParam(':timestamp',$line['timestamp']);
-			$insert->bindParam(':session_id',$line['session_id']);
-			$insert->bindParam(':record_id',$line['record_id']);
-			$insert->bindParam(':name',$line['name']);
-			$insert->bindParam(':workout',$line['workout']);
-			$insert->bindParam(':accel_x',$line['accel_x']);
-			$insert->bindParam(':accel_y',$line['accel_y']);
-			$insert->bindParam(':accel_z',$line['accel_z']);
-			$insert->bindParam(':orient_x',$line['orient_x']);
-			$insert->bindParam(':orient_y',$line['orient_y']);
-			$insert->bindParam(':orient_z',$line['orient_z']);
-			$insert->bindParam(':orient_w',$line['orient_w']);
-			$insert->bindParam(':rotate_x',$line['rotate_x']);
-			$insert->bindParam(':rotate_y',$line['rotate_y']);
-			$insert->bindParam(':rotate_z',$line['rotate_z']);
+		foreach($json_data['data'] as $sensorData) {
+			$insert->bindParam(':timestamp',$sensorData['timestamp']);
+			$insert->bindParam(':session_id',$sensorData['session_id']);
+			$insert->bindParam(':record_id',$sensorData['record_id']);
+			$insert->bindParam(':name',$sensorData['name']);
+			$insert->bindParam(':workout',$sensorData['workout']);
+			$insert->bindParam(':accel_x',$sensorData['Acceleration']['X']);
+			$insert->bindParam(':accel_y',$sensorData['Acceleration']['Y']);
+			$insert->bindParam(':accel_z',$sensorData['Acceleration']['Z']);
+			$insert->bindParam(':orient_x',$sensorData['Orientation']['X']);
+			$insert->bindParam(':orient_y',$sensorData['Orientation']['Y']);
+			$insert->bindParam(':orient_z',$sensorData['Orientation']['Z']);
+			$insert->bindParam(':orient_w',$sensorData['Orientation']['W']);
+			$insert->bindParam(':rotate_x',$sensorData['Rotation']['X']);
+			$insert->bindParam(':rotate_y',$sensorData['Rotation']['Y']);
+			$insert->bindParam(':rotate_z',$sensorData['Rotation']['Z']);
 
 			$insert->execute();
 		}
@@ -42,14 +41,5 @@
 
 	} catch (PDOException $e) {
 		echo "Error: ".$e->getMessage();
-	}
-
-
-	function hexToStr($hex){
-		$string='';
-		for ($i=0; $i < strlen($hex)-1; $i+=2){
-			$string .= chr(hexdec($hex[$i].$hex[$i+1]));
-		}
-		return $string;
 	}
 ?>
