191ce1b4f70e8ec53292949df23a6082a2407a11
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2020-06-12 01:04:04 -0500

    Update download.php

--- download.php
+++ download.php
@@ -5,18 +5,22 @@
 
 	try {
 		$pdo = new PDO($conn_string, $configs['username'], $configs['password']);
-
-		$stmt = $pdo->prepare("SELECT * FROM sensors;");
-
+		
+		$delimiter = ",";
+		$filename = "SensorData_" . date('Y-m-d') . ".csv";
+		$f = fopen('php://memory', 'w');
+		
+		$stmt = $pdo->prepare("SELECT column_name FROM information_schema.columns WHERE table_name = 'sensors';");
 		if($stmt->execute()) {
-			$delimiter = ",";
-			$filename = "SensorData_" . date('Y-m-d') . ".csv";
-      
-			$f = fopen('php://memory', 'w');
-      
-			$fields = array('timestamp','session_id','record_id','name','workout','accel_x','accel_y','accel_z','orient_x','orient_y','orient_z','orient_w','rotate_x','rotate_y','rotate_z');
-			fputcsv($f, $fields, $delimiter);
-    
+			$columns = array();
+			while($row = $stmt->fetch()){
+				array_push($columns, $row[0]);
+			}
+			fputcsv($f, $columns, $delimiter);
+		}
+		
+		$stmt = $pdo->prepare("SELECT * FROM sensors;");
+		if($stmt->execute()) {
 			while($row = $stmt->fetch(PDO::FETCH_NUM)){
 				$lineData = array();
 				for($i = 0; $i < count($row); $i++) {
