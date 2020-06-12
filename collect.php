c4b0bca60f8d37d10b65f0e1b7b7e23ab6e429b2
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2020-06-12 01:13:43 -0500

    Corrected heart_rate data type

--- collect.php
+++ collect.php
@@ -9,7 +9,7 @@
 		// Create sensors table, 
 		// Computer rotation: orient_x/y/z/w
 		// Human rotation in degrees rotate_x/y/z
-		$myPDO->exec('CREATE TABLE sensors (timestamp bigint, session_id bigint, record_id bigint, name text, workout text, heart_rate tinyint, accel_x real, accel_y real, accel_z real, orient_x real, orient_y real, orient_z real, orient_w real, rotate_x real, rotate_y real, rotate_z real)');
+		$myPDO->exec('CREATE TABLE sensors (timestamp bigint, session_id bigint, record_id bigint, name text, workout text, heart_rate smallint, accel_x real, accel_y real, accel_z real, orient_x real, orient_y real, orient_z real, orient_w real, rotate_x real, rotate_y real, rotate_z real)');
 
 		$insert_string = 'INSERT INTO sensors (timestamp, session_id, record_id, name, workout, heart_rate, accel_x, accel_y, accel_z, orient_x, orient_y, orient_z, orient_w, rotate_x, rotate_y, rotate_z) VALUES (:timestamp, :session_id, :record_id, :name, :workout, :heart_rate, :accel_x, :accel_y, :accel_z, :orient_x, :orient_y, :orient_z, :orient_w, :rotate_x, :rotate_y, :rotate_z)';
 		$insert = $myPDO->prepare($insert_string);
