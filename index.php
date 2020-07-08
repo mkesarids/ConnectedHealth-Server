c4dfaf749916578f0cdbcdae78f6375da479354c
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2020-07-08 15:48:23 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,41 @@
+<?php
+	function displayForm() {
+		echo '<html>';
+		echo '<body>';
+		echo '<h2>ConnectedHealth Data</h2>';
+		echo '<form method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>">';
+		echo '<table>';
+		echo '<tr><td>Session id: </td><td><input type="text" name="session_id"></td></tr>';
+		echo '<tr><td>Name: </td><td><input type="text" name="name"></td></tr>';
+		echo '<tr><td>Workout: </td><td><input type="text" name="workout"></td></tr>';
+		echo '<tr><td><input type="submit" name="action" value="Display"></td>';
+		echo '<td><input type="submit" name="action" value="Download"></td></tr>';
+		echo '</table>';
+		echo '</form>';
+	}
 
-<!DOCTYPE html> 
-<html>
+	if(!isset($_POST["action"])) {
+		displayForm();
+	} else {
+		// Defining variables
+		$session_id = $name = $workout = $action = "";
 
-<head>
-</head>
+        		// Checking for a POST request
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			$session_id = test_input($_POST["session_id"]);
+			$name = test_input($_POST["name"]);
+			$workout = test_input($_POST["workout"]);
+			$action = test_input($_POST["action"]);
+		}
 
-<body>
-    <?php
-        // Defining variables
-	$session_id = $name = $workout = $action = "";
+		// Removing the redundant HTML characters if any exist.
+		function test_input($data) {
+			$data = trim($data);
+			$data = stripslashes($data);
+			$data = htmlspecialchars($data);
+			return $data;
+		}
 
-        // Checking for a POST request
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-		$session_id = test_input($_POST["session_id"]);
-		$name = test_input($_POST["name"]);
-		$workout = test_input($_POST["workout"]);
-		$action = test_input($_POST["action"]);
-        }
-
-        // Removing the redundant HTML characters if any exist.
-        function test_input($data) {
-          $data = trim($data);
-          $data = stripslashes($data);
-          $data = htmlspecialchars($data);
-          return $data;
-        }
-        ?>
-
-        <h2>ConnectedHealth Data Download</h2>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>">
-		<table>
-			<tr><td>Session id: </td><td><input type="text" name="session_id"></td></tr>
-			<tr><td>Name: </td><td><input type="text" name="name"></td></tr>
-			<tr><td>Workout: </td><td><input type="text" name="workout"></td></tr>
-			<tr><td><input type="submit" name="action" value="Display"></td>
-			<td><input type="submit" name="action" value="Download"></td></tr>
-		</table>
-        </form>
-
-        <?php
 		// Connection details
 		$configs = include('config.php');
 		$conn_string = 'pgsql:host='.$configs['host'].';dbname='.$configs['dbname'];
@@ -47,7 +44,7 @@
 			$pdo = new PDO($conn_string, $configs['username'], $configs['password']);
 
 			$column_stmt = $pdo->prepare("SELECT column_name FROM information_schema.columns WHERE table_name = 'sensors';");
-			
+		
 			$values_stmt = $pdo->prepare("SELECT * FROM sensors WHERE name LIKE :name AND workout LIKE :workout;");
 			$values_stmt->bindValue(":name", "%{$name}%");
 			$values_stmt->bindValue(":workout", "%{$workout}%");
@@ -76,8 +73,8 @@
 			} else if($action === "Download") {
 				$delimiter = ",";
 				$filename = "SensorData_" . date('Y-m-d') . ".csv";
-				$f = fopen($_SERVER['DOCUMENT_ROOT'] . '/tmp.csv', 'w');
-				
+				$f = fopen('php://memory', 'w');
+
 				if($column_stmt->execute()) {
 					$columns = array();
 					while($row = $column_stmt->fetch()){
@@ -94,20 +91,17 @@
 						}
 						fputcsv($f, $lineData, $delimiter);
 					}
-					
+				
 					fseek($f, 0);
-					
+				
 					header('Content-Type: text/csv');
 					header('Content-Disposition: attachment; filename="' . $filename . '";');
-					
+				
 					fpassthru($f);
 				}
-				fclose($f);
 			}
 		} catch (PDOException $e) {
 			echo "Error: ".$e->getMessage();
 		}
-        ?>
-</body> 
-
-</html>
+	}
+?>
