fb86f7d133326f70020943f60277ee657d13cbbd
JChamblee99 <jtc38@students.uwf.edu>
2020-11-26 06:38:39 -0600

    Updated credentials

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 	return array(
-		'host' => 'ec2-18-206-84-251.compute-1.amazonaws.com',
-		'username' => 'tljymawvctthhl',
-		'password' => '686e4e04d04863fa587a3843cdf69e943f3cd77ce5a12d17c10221a61f0e320b',
-		'dbname' => 'd33bcpvj60ocm8'
+		'host' => 'ec2-35-168-77-215.compute-1.amazonaws.com',
+		'username' => 'obetgrwgqzcuxt',
+		'password' => 'c3fed1b95867565f00c68396658a6f78fbcb040253bcc0a979f8c8e3501ab537',
+		'dbname' => 'd8f7944mbof2o3'
 	)
 ?>
