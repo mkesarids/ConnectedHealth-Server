6748720294c440550bded5ddb24e005bba434be9
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2021-04-09 22:24:34 -0500

    Removed the unused record_id

--- collect.php
+++ collect.php
@@ -9,9 +9,9 @@
 		// Create sensors table, 
 		// Computer rotation: orient_x/y/z/w
 		// Human rotation in degrees rotate_x/y/z
-		$myPDO->exec('CREATE TABLE sensors (timestamp bigint, session_id bigint, record_id bigint, name text, age bigint, gender text, height bigint, workout text, workout_description text, heart_rate smallint, accel_x real, accel_y real, accel_z real, euler_orient_x real, euler_orient_y real, euler_orient_z real, quat_orient_x real, quat_orient_y real, quat_orient_z real, quat_orient_w real, rotation_x real, rotation_y real, rotation_z real)');
+		$myPDO->exec('CREATE TABLE sensors (timestamp bigint, session_id bigint, name text, age bigint, gender text, height bigint, workout text, workout_description text, heart_rate smallint, accel_x real, accel_y real, accel_z real, euler_orient_x real, euler_orient_y real, euler_orient_z real, quat_orient_x real, quat_orient_y real, quat_orient_z real, quat_orient_w real, rotation_x real, rotation_y real, rotation_z real)');
 
-		$insert_string = 'INSERT INTO sensors (timestamp, session_id, record_id, name, age, gender, height, workout, workout_description, heart_rate, accel_x, accel_y, accel_z, euler_orient_x, euler_orient_y, euler_orient_z, quat_orient_x, quat_orient_y, quat_orient_z, quat_orient_w, rotation_x, rotation_y, rotation_z) VALUES (:timestamp, :session_id, :record_id, :name, :age, :gender, :height, :workout, :workout_description, :heart_rate, :accel_x, :accel_y, :accel_z, :euler_orient_x, :euler_orient_y, :euler_orient_z, :quat_orient_x, :quat_orient_y, :quat_orient_z, :quat_orient_w, :rotation_x, :rotation_y, :rotation_z)';
+		$insert_string = 'INSERT INTO sensors (timestamp, session_id, name, age, gender, height, workout, workout_description, heart_rate, accel_x, accel_y, accel_z, euler_orient_x, euler_orient_y, euler_orient_z, quat_orient_x, quat_orient_y, quat_orient_z, quat_orient_w, rotation_x, rotation_y, rotation_z) VALUES (:timestamp, :session_id, :name, :age, :gender, :height, :workout, :workout_description, :heart_rate, :accel_x, :accel_y, :accel_z, :euler_orient_x, :euler_orient_y, :euler_orient_z, :quat_orient_x, :quat_orient_y, :quat_orient_z, :quat_orient_w, :rotation_x, :rotation_y, :rotation_z)';
 		$insert = $myPDO->prepare($insert_string);
 
 		// Decoding JSON and collecting the data
@@ -20,7 +20,6 @@
 		foreach($json_data['data'] as $sensorData) {
 			$insert->bindParam(':timestamp',$sensorData['Timestamp']);
 			$insert->bindParam(':session_id',$sensorData['session_id']);
-			$insert->bindParam(':record_id',$sensorData['record_id']);
 			$insert->bindParam(':name',$sensorData['name']);
 			$insert->bindParam(':age',$sensorData['age']);
 			$insert->bindParam(':gender',$sensorData['gender']);
