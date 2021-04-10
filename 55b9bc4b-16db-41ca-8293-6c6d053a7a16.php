7ba59049350eab389639386341679de2e46594db
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2021-04-09 23:49:11 -0500

    Corrected field names in the SQL interface

--- 55b9bc4b-16db-41ca-8293-6c6d053a7a16.php
+++ 55b9bc4b-16db-41ca-8293-6c6d053a7a16.php
@@ -2,7 +2,7 @@
 <form method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>">
 	<table>
 		<tr><td>Password: </td><td><input type="password" name="password"></td></tr>
-		<tr><td>Name: </td><td><textarea name="sql" rows="4" cols="50"></textarea></td></tr>
+		<tr><td>Query: </td><td><textarea name="query" rows="4" cols="50"></textarea></td></tr>
 		<td><input type="submit" name="action" value="Execute"></td></tr>
 	</table>
 </form>
@@ -16,7 +16,7 @@
 
 		try {
 			$myPDO = new PDO($conn_string, $configs['username'], $_POST['password']);
-			$myPDO->exec($_POST['sql']);
+			$myPDO->exec($_POST['query']);
 		} catch (PDOException $e) {
 			echo "Error";
 		}
