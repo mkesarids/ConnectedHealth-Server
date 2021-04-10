182c2842924c9bea0b6cc36313406e2ba5fae460
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2021-04-09 22:56:04 -0500

    Explicitly defined GET and POST in SQL interface

--- 55b9bc4b-16db-41ca-8293-6c6d053a7a16.php
+++ 55b9bc4b-16db-41ca-8293-6c6d053a7a16.php
@@ -1,23 +1,23 @@
-<?php if($_SERVER["REQUEST_METHOD"] == "GET" ?>
-	<h2>Connected Health Data Form</h2>
-	<form method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>">
-		<table>
-			<tr><td>Password: </td><td><input type="password" name="password"></td></tr>
-			<tr><td>Name: </td><td><textarea name="sql" rows="4" cols="50"></textarea></td></tr>
-			<td><input type="submit" name="action" value="Execute"></td></tr>
-		</table>
-	</form>
-<?php endif; ?>
-
 <?php
-	// Connection details
-	$configs = include('config.php');
-	$conn_string = 'pgsql:host='.$configs['host'].';dbname='.$configs['dbname'];
+	if($_SERVER["REQUEST_METHOD"] == "GET") {
+		<h2>Connected Health Data Form</h2>
+		<form method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>">
+			<table>
+				<tr><td>Password: </td><td><input type="password" name="password"></td></tr>
+				<tr><td>Name: </td><td><textarea name="sql" rows="4" cols="50"></textarea></td></tr>
+				<td><input type="submit" name="action" value="Execute"></td></tr>
+			</table>
+		</form>
+	} else if($_SERVER["REQUEST_METHOD"] == "POST") {
+		// Connection details
+		$configs = include('config.php');
+		$conn_string = 'pgsql:host='.$configs['host'].';dbname='.$configs['dbname'];
 
-	try {
-		$myPDO = new PDO($conn_string, $configs['username'], $_POST['password']);
-		$myPDO->exec($_POST['sql']);
-	} catch (PDOException $e) {
-		echo "Error";
+		try {
+			$myPDO = new PDO($conn_string, $configs['username'], $_POST['password']);
+			$myPDO->exec($_POST['sql']);
+		} catch (PDOException $e) {
+			echo "Error";
+		}
 	}
 ?>
