807abf95de187ecb414e17039013befc67e053e7
John Chamblee <42752805+JChamblee99@users.noreply.github.com>
2021-04-09 22:58:55 -0500

    Restructured request control structure

--- 55b9bc4b-16db-41ca-8293-6c6d053a7a16.php
+++ 55b9bc4b-16db-41ca-8293-6c6d053a7a16.php
@@ -1,14 +1,16 @@
+<?php if($_SERVER["REQUEST_METHOD"] == "GET" | $_POST["action"] === "Display"): ?>
+	<h2>Connected Health Data Form</h2>
+	<form method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>">
+		<table>
+			<tr><td>Password: </td><td><input type="password" name="password"></td></tr>
+			<tr><td>Name: </td><td><textarea name="sql" rows="4" cols="50"></textarea></td></tr>
+			<td><input type="submit" name="action" value="Execute"></td></tr>
+		</table>
+	</form>
+<?php endif; ?>
+
 <?php
-	if($_SERVER["REQUEST_METHOD"] == "GET") {
-		<h2>Connected Health Data Form</h2>
-		<form method="post" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]);?>">
-			<table>
-				<tr><td>Password: </td><td><input type="password" name="password"></td></tr>
-				<tr><td>Name: </td><td><textarea name="sql" rows="4" cols="50"></textarea></td></tr>
-				<td><input type="submit" name="action" value="Execute"></td></tr>
-			</table>
-		</form>
-	} else if($_SERVER["REQUEST_METHOD"] == "POST") {
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		// Connection details
 		$configs = include('config.php');
 		$conn_string = 'pgsql:host='.$configs['host'].';dbname='.$configs['dbname'];
